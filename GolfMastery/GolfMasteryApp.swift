e779b4bece79401d88fc566e70683e251779f554
Matt Deuschle <mdeuschle@gmail.com>
2026-04-19 17:34:50 -0700

    Initial commit: GolfMastery iOS app setup

---
+++ GolfMastery/GolfMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfMasteryApp.swift
+//  GolfMastery
+//
+//  Created by Matt Deuschle on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct GolfMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
